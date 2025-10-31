734887597202366d7a1a1fb461f60dc54bd509eb
Afnan Asilah <afnanyogs@gmail.com>
2025-10-31 10:42:38 +0700

    menambahkan tombol submit

--- app/src/main/java/com/example/questuserinput_153/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_153/ui/theme/TeksEdit.kt
@@ -79,3 +79,30 @@ fun FormDataDiri(modifier: Modifier
             }
         }
 
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Alamat Lengkap") },
+
+            onValueChange = {
+                textAlamat = it
+            }
+        )
+
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium)),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            enabled = textAlamat.isNotEmpty(),
+            onClick = {
+                nama=textNama
+                jenis=textJK
+                alamat=textAlamat
+            }
+        )
